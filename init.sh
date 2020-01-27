5e4f08fcb63758b33f13b090992b1438bd6327b7
Grégory Ninforge <gregory.ninforge@gmail.com>
2020-01-27 22:15:41 +0100

    fix timemachine

--- init.sh
+++ init.sh
@@ -4,18 +4,14 @@ if [ ! -f /config/config.yaml ]; then
     exit 0;
 fi
 
-echo verification
-
 pykwalify -s /tmp/schema.yaml -d /config/config.yaml
 if [ $? -ne 0 ]; then
     echo config.yaml is not correct
     exit 0;
 fi
 
-echo execution du fichier
-
 j2 /tmp/setup.sh.j2 /config/config.yaml | sh
 
 j2 /tmp/smb.conf.j2 /config/config.yaml > /etc/samba/smb.conf
 
-#exec "${@}"
+exec "${@}"
